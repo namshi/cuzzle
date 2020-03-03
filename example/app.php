aeee035edc9e0884b37649147e2242f9b555e751
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-03-04 00:13:49 +0600

    updated example

--- example/app.php
+++ example/app.php
@@ -3,16 +3,18 @@
 require '../vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use Namshi\Cuzzle\Subscriber\CurlFormatterSubscriber;
-use Monolog\Logger;
+use GuzzleHttp\HandlerStack;
 use Monolog\Handler\TestHandler;
+use Monolog\Logger;
+use Namshi\Cuzzle\Middleware\CurlFormatterMiddleware;
 
 $logger = new Logger('guzzele.to.curl'); //initialize the logger
 $testHandler = new TestHandler(); //test logger handler
 $logger->pushHandler($testHandler);
 
-$client  = new Client(); //initialize a Guzzle client
-$client->getEmitter()->attach(new CurlFormatterSubscriber($logger)); //add the cURL formatter subscriber
+$handlerStack = HandlerStack::create();
+$handlerStack->push(new CurlFormatterMiddleware($logger), 'logger');
+$client = new Client([ 'handler' => $handlerStack ]); //initialize a Guzzle client
 
 $response = $client->get('http://google.com'); //let's fire a request
 
